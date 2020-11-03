dfb3308bd7a373bb77d9a65410c47d3dd434500f
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-02 19:21:48 -0800

    initial commit

---
+++ data/load-seed-data.js
@@ -0,0 +1,47 @@
+const client = require('../lib/client');
+// import our seed data:
+const rogues = require('./rogues.js');
+const usersData = require('./users.js');
+const { getEmoji } = require('../lib/emoji.js');
+
+run();
+
+async function run() {
+
+  try {
+    await client.connect();
+
+    const users = await Promise.all(
+      usersData.map(user => {
+        return client.query(`
+                      INSERT INTO users (email, hash)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [user.email, user.hash]);
+      })
+    );
+      
+    const user = users[0].rows[0];
+
+    await Promise.all(
+      rogues.map(rogue => {
+        return client.query(`
+                    INSERT INTO rogues (alias, name, alive, category)
+                    VALUES ($1, $2, $3, $4);
+                `,
+        [rogue.name, rogue.cool_factor, user.id]);
+      })
+    );
+    
+
+    console.log('seed data load complete', getEmoji(), getEmoji(), getEmoji());
+  }
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    client.end();
+  }
+    
+}
---
+++ data/rogues.js
@@ -0,0 +1,247 @@
+module.exports = [
+  {
+    alias: 'Bane',
+    name: 'Edward',
+    alive: true,
+    category: 'Central Rogue',
+    year: 1993
+  },
+  {
+    alias: 'Black Mask',
+    name: 'Roman Sionis',
+    alive: false,
+    category: 'Central Rogues',
+    year: 1985
+  },
+  {
+    alias: 'The Boss',
+    name: 'Sal Maroni',
+    alive: false,
+    category: 'Organized Crime',
+    year: 1942
+  },
+  {
+    alias: 'Catwoman',
+    name: 'Selina Kyle',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1940
+  },
+  {
+    alias: 'Calendar Man',
+    name: 'Julian Gregory Day',
+    alive: true,
+    category: 'Secondary Rogues',
+    year: 1958
+  },
+  {
+    alias: 'Clayface',
+    name: 'Basil Karlo',
+    alive: false,
+    category: 'Central Rogues',
+    year: 1940
+  },
+  {
+    alias: 'Deadshot',
+    name: 'Floyd Lawton',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1950
+  },
+  {
+    alias: 'Deathstroke',
+    name: 'Slade Wilson',
+    alive: false,
+    category: 'Central Rogues',
+    year: 1980
+  },
+  {
+    alias: 'Firefly',
+    name: 'Garfield Lynns',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1952
+  },
+  {
+    alias: 'Great White Shark',
+    name: 'Warren White',
+    alive: true,
+    category: 'Secondary Rogues',
+    year: 2004
+  },
+  {
+    alias: 'Hangman',
+    name: 'Sofia Gigante',
+    alive: false,
+    category: 'Organized Crime',
+    year: 1999
+  },
+  {
+    alias: 'Harley Quinn',
+    name: 'Dr. Harleen F. Quinzel',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1992
+  },
+  {
+    alias: 'Holiday',
+    name: 'Alberto Falcone',
+    alive: false,
+    category: 'Organized Crime',
+    year: 1996
+  },
+  {
+    alias: 'Hugo Strange',
+    name: 'Professor Hugo Strange',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1940
+  },
+  {
+    alias: 'Hush',
+    name: 'Dr. Thomas Elliot',
+    alive: true,
+    category: 'Central Rogues',
+    year: 2003
+  },
+  {
+    alias: 'The Joker',
+    name: 'Jack Napier',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1940
+  },
+  {
+    alias: 'Killer Croc',
+    name: 'Waylon Jones',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1983
+  },
+  {
+    alias: 'Mad Hatter',
+    name: 'Dr. Jervis Tetch',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1948
+  },
+  {
+    alias: 'Mr. Freeze',
+    name: 'Dr. Victor Fries',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1959
+  },
+  {
+    alias: 'Onomatopoeia',
+    name: 'N/A',
+    alive: true,
+    category: 'Recurring Rogues',
+    year: 2002
+  },
+  {
+    alias: 'The Penguin',
+    name: 'Oswald Chesterfield Cobblepot',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1941
+  },
+  {
+    alias: 'Poison Ivy',
+    name: 'Dr. Pamela Lillian Isley',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1966
+  },
+  {
+    alias: 'Ra\'s al Ghul',
+    name: 'Ra\'s al Ghul',
+    alive: true,
+    category: 'League of Assassins',
+    year: 1971
+  },
+  {
+    alias: 'The Red Hood',
+    name: 'Jason Todd',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1987
+  },
+  {
+    alias: 'The Riddler',
+    name: 'Edward Nigma/Nashton',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1948
+  },
+  {
+    alias: 'The Roman',
+    name: 'Carmine Falcone',
+    alive: false,
+    category: 'Organized Crime',
+    year: 1987
+  },
+  {
+    alias: 'Two-Face',
+    name: 'Harvey Dent',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1942
+  },
+  {
+    alias: 'Man-Bat',
+    name: 'Dr. Kirk Langstrom',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1970
+  },
+  {
+    alias: 'Maxie Zeus',
+    name: 'Maximillian Zeus',
+    alive: true,
+    category: 'Secondary Rogues',
+    year: 1979
+  },
+  {
+    alias: 'Mr. Zsasz',
+    name: 'Victor Zsasz',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1992
+  },
+  {
+    alias: 'Professor Pyg',
+    name: 'Dr. Lazlo Valentin',
+    alive: true,
+    category: 'Secondary Rogues',
+    year: 2007
+  },
+  {
+    alias: 'Scarecrow',
+    name: 'Professor Jonathan Crane',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1941
+  },
+  {
+    alias: 'Solomon Grundy',
+    name: 'Cyrus Gold',
+    alive: false,
+    category: 'Secondary Rogue',
+    year: 1944
+  },
+  {
+    alias: 'Talia al Ghul',
+    name: 'Talia al Ghul',
+    alive: true,
+    category: 'League of Assassins',
+    year: 1971
+  },
+  {
+    alias: 'Ventriloquist',
+    name: 'Arnold Wesker',
+    alive: true,
+    category: 'Central Rogues',
+    year: 1988
+  }
+];
