b218a4dbfb4f72dfd4bc31a95eee50079b030796
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-02 19:48:58 -0800

    database tweaks?

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,7 +31,7 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-  test('returns rogues', async() => {
+  test('returns animals', async() => {
 
     const expectation = [
       {
@@ -55,7 +55,7 @@ describe('app routes', () => {
     ];
 
     const data = await fakeRequest(app)
-      .get('/rogues')
+      .get('/animals')
       .expect('Content-Type', /json/)
       .expect(200);
 
