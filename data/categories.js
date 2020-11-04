0054b4bd8b90d8ff945987071f55a764f9679b8c
Sjaan Hydrick <sjaanahydrick@gmail.com>
2020-11-04 14:54:49 -0800

    added second table

---
+++ data/categories.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'Central Rogues'
+  },
+  {
+    name: 'Organized Crime'
+  },
+  {
+    name: 'Secondary Rogues'
+  },
+  {
+    name: 'Recurring Rogues'
+  },
+  {
+    name: 'League of Assassins'
+  }
+];
